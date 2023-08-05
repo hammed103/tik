521f7e46d7c7ff011a9cd8ecc84b1bfa76474657
hammed103 <hammedfree@gmail.com>
2023-08-05 13:46:02 +0100

    start

--- tik/base/views.py
+++ tik/base/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from django.http import JsonResponse
+from django.http import JsonResponse,HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 import json
@@ -9,6 +9,13 @@ import json
 @require_POST
 def slack_events(request):
     data = json.loads(request.body)
+
+    # Handle URL verification
+    if data['type'] == 'url_verification':
+        return HttpResponse(data['challenge'])
+
+    # Handle other event types
     if data['type'] == 'event_callback' and data['event']['type'] == 'message':
         print(f"Received message: {data['event']['text']}")
     return JsonResponse({'status': 'ok'})
+
